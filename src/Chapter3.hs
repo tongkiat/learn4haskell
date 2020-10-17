03c530db820158d19049e72b8ed19610f224ae4e
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2020-10-17 12:27:31 +0800

    Add task 1

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -344,6 +344,12 @@ of a book, but you are not limited only by the book properties we described.
 Create your own book type of your dreams!
 -}
 
+data Book = Book
+    { bookTitle  :: String
+    , bookAuthor :: String
+    , bookPages  :: Int
+    }
+
 {- |
 =⚔️= Task 2
 
