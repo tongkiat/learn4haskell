63db85ad4369c46811b7c119b94b8420be70ac75
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2020-10-17 12:10:34 +0800

    Add task 9

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -777,7 +777,8 @@ the list with only those lists that contain a passed element.
 🕯 HINT: Use the 'elem' function to check whether an element belongs to a list
 -}
 
-contains = error "contains: Not implemented!"
+contains :: Int -> [[Int]] -> [[Int]]
+contains x = filter (elem x)
 
 {- |
 =🛡= Eta-reduction
