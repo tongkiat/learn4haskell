b7d21961a05925272b30e2d418f15e46ee5f29a9
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2020-10-27 13:13:09 +0800

    Update task 3

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -482,7 +482,12 @@ Create a simple enumeration for the meal types (e.g. breakfast). The one who
 comes up with the most number of names wins the challenge. Use your creativity!
 -}
 
-data Meal = Breakfast | Brunch | Lunch | Dinner
+data Meal
+    = Breakfast
+    | Brunch
+    | Lunch
+    | Dinner
+    | Supper
 
 {- |
 =⚔️= Task 4
